c0c4cadb882857271acddd5636227ffe790d8aaa
amin taghipour <taghipur.amin@gmail.com>
2022-03-01 02:38:18 +0330

    config index page to access to product page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { Fragment } from 'react'
+import styles from "styles/Home.module.scss"
 
 
 export default function Home() {
   return (
-    <div>
+    <Fragment>
       <Head>
+        <title>behtarino</title>
+        <meta name="description" content="the interview test" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-    </div>
+      <div className={`${styles.wrapper} d-flex justify-content-center align-items-center`}>
+        <strong>
+          <Link href="/products">
+            List of Products
+          </Link>
+        </strong>
+      </div>
+    </Fragment >
   )
 }
